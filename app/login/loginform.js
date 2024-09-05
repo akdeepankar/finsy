0ff334474a92771ea8c0c0dbfa7bed2317d4bf79
grissharrisdennis <21f3001987@ds.study.iitm.ac.in>
2024-09-06 02:32:20 +0530

    update login and signup form

---
+++ app/login/loginform.js
@@ -0,0 +1,103 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+const LoginForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  
+  const handleLogin = (data) => console.log(data);
+  const handleError = (errors) => console.log(errors);
+
+  const loginOptions = {
+    email: { required: "Email is required" },
+    password: {
+      required: "Password is required",
+      minLength: {
+        value: 8,
+        message: "Password must have at least 8 characters"
+      }
+    }
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: '480px',
+        minWidth: '320px',
+        padding: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        border: '1px solid #ddd',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        margin: '0 auto', 
+      }}
+    >
+      <h1 style={{ textAlign: 'center',fontSize:'32px', fontWeight: 'bold' }}>
+        Login
+      </h1>
+      <br></br>
+      <br></br>
+      <form onSubmit={handleSubmit(handleLogin, handleError)}>
+        <div style={{ marginBottom: '20px' }}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            {...register('email', loginOptions.email)}
+            style={{
+              width: '100%',
+              padding: '10px',
+              border: '1px solid',
+              borderColor: errors?.email ? 'red' : '#ccc',
+              borderRadius: '4px',
+            }}
+          />
+          {errors?.email && (
+            <p style={{ color: 'red' }}>{errors.email.message}</p>
+          )}
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            {...register('password', loginOptions.password)}
+            style={{
+              width: '100%',
+              padding: '10px',
+              border: '1px solid',
+              borderColor: errors?.password ? 'red' : '#ccc',
+              borderRadius: '4px',
+            }}
+          />
+          {errors?.password && (
+            <p style={{ color: 'red' }}>{errors.password.message}</p>
+          )}
+        </div>
+        <br></br>
+        <button
+          type="submit"
+          style={{
+            backgroundColor: '#0070f3',
+            color: '#fff',
+            padding: '10px',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            width: '100%',
+          }}
+        >
+          Submit
+        </button>
+        <br></br>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
+
+
+
+    
--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+'use client'; 
 import Image from "next/image";
 import Spline from '@splinetool/react-spline/next';
 import { Input, Button } from "@nextui-org/react";
 import { MailIcon } from './MailIcon';
+
 
 export default function Home() {
   return (
@@ -52,6 +54,7 @@ export default function Home() {
           />
         </div>
       </section>
+
 
       {/* About Section - Visible only on Mobile */}
       <section id="about" className="block md:hidden items-center justify-center min-h-screen bg-black dark:bg-black text-center p-4">
